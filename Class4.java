08ee34f22bcfed6c69b6de6177c7133eb2f6d371
Sapna1982 <sapnasharma700@hotmail.com>
2022-12-03 14:44:45 +0000

    homework2

---
+++ Class4.java
@@ -0,0 +1,13 @@
+package homework2;
+
+public class Class4 {public static void main(String[] args)
+{System.out.println("Static Vs Dynamic Testing");
+System.out.println("Exploratory Testing");
+System.out.println("Adhoc Testing");
+System.out.println("Waterfall");
+System.out.println("Iterative");
+System.out.println("V Model");
+System.out.println("Verification and Validation");
+System.out.println("Agile");
+System.out.println("Definition of Ready/Done");}
+}
---
+++ Class5.java
@@ -0,0 +1,10 @@
+package homework2;
+
+public class Class5 {public static void main(String[] args)
+{System.out.println("What IS TDD");
+System.out.println("What Is BDD");
+System.out.println("BDD Keywords");
+System.out.println("How To Write Test Cases With BDD");
+System.out.println("Maintenance");
+}
+}
